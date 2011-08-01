11b7dce71025a57907f3945cf8b92f2939f4caed
Hannes Landeholm <hannes@recurringly.com>
2011-08-01 03:28:39 +0200

    Adding default melt project files.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Contains the configuration for this application.
+ *
+ * Melt Framework configuration options are named constants that resides in the
+ * namespace \melt\$MODULE\config where $MODULE is the module that uses the
+ * configuration option.
+ *
+ * Melt Framework will automatically insert missing directives into this file
+ * so don't be suprised when it starts editing itself.
+ */
